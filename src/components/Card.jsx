6f75450fce3b152410655a3bae64b3a89e9b79e6
thierry turlet <jawadgwada@hotmail.fr>
2025-03-21 22:34:12 +0200

    mise en place des cards

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+
+
+const Card = ({ text }) => {
+    return (
+      <div className="card-home">
+        <p className="card__text">{text}</p>
+      </div>
+    );
+  };
+  
+  export default Card;
+  
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,29 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Banner from '../components/Banner'; 
 import imgbanner from '../assets/imgbanner.png';
+import Card from '../components/Card';
 
 const Home = () => {
   return (
     <>
       <Navbar />
- 
       <Banner text="Chez vous, partout et ailleurs" image={imgbanner} />
       
       <div className="main">
         <h1>Home</h1>
+      
+
+        <div className="cards-container">
+          <Card text="titre de la location" />
+          <Card text="titre de la location" />
+          <Card text="titre de la location" />
+          <Card text="titre de la location" />
+          <Card text="titre de la location" />
+          <Card text="titre de la location" />
+        </div>
       </div>
-      <Footer opacity={0.6} />
+
+      <Footer />
     </>
   );
 };
